15660f37724f066419d053299a4a80f0fd299819
Luke Shiner <luke@lukeshiner.com>
2017-01-26 11:53:54 +0000

    Skulink uses argument format of Status

--- linking/skulink.py
+++ linking/skulink.py
@@ -24,36 +24,56 @@ class SKULink(Command):
     def make_parser(self):
         """Create argument parser and add arguments."""
         self.parser = argparse.ArgumentParser(description=self.description)
-        self.parser.add_argument('-s', '--subsource', required=True, type=str)
+        self.parser.add_argument(
+            'channels', nargs='?', default=None, type=str)
 
     def get_args(self):
         """Get arguments from self.parser."""
         self.args = super().get_args()
-        try:
-            self.sub_source = stclocal.sub_source_lookup(self.args.subsource)
-        except:
-            print('Channel not found')
-            exit(1)
+        if self.args.channels is None:
+            source = None
+            sub_source = None
+        else:
+            try:
+                source = stclocal.source_lookup(self.args.channels)
+            except stclocal.ChannelNotFound:
+                try:
+                    sub_source = stclocal.sub_source_lookup(self.args.channels)
+                except:
+                    print('Channel not found')
+                    exit(1)
+                source = None
+            else:
+                sub_source = None
+        self.channels = stclocal.pylinnworks.Linking(
+            sub_source=sub_source, source=source)
 
     def main(self):
         """Link items by SKU."""
-        channel = stclocal.pylinnworks.Linking.get_channel_by_sub_source(
-            sub_source=self.sub_source)
+        self.linked_count = 0
+        self.not_linked_count = 0
+        for channel in self.channels:
+            self.skulink_channel(channel)
+        print('{} items linked.'.format(self.linked_count))
+        print('{} items failed to link.'.format(self.not_linked_count))
+
+    def skulink_channel(self, channel):
+        """Link channel by SKU."""
         items = channel.get_items(linked=False)
-        linked_count = 0
-        not_linked_count = 0
         for item in items:
-            try:
-                linn_item = stclocal.pylinnworks.Inventory.get_item_by_SKU(
-                    item.sku)
-            except:
-                not_linked_count += 1
-                continue
-            else:
-                self.log('{} {} item {} linked to {}: {}'.format(
-                    item.source, item.sub_source, item.sku, linn_item.sku,
-                    linn_item.stock_id))
-                item.link(linn_item.stock_id)
-                linked_count += 1
-        print('{} items linked.'.format(linked_count))
-        print('{} items failed to link.'.format(not_linked_count))
+            self.skulink_item(item)
+
+    def skulink_item(self, item):
+        """Link item by SKU."""
+        try:
+            linn_item = stclocal.pylinnworks.Inventory.get_item_by_SKU(
+                item.sku)
+        except:
+            self.not_linked_count += 1
+            return
+        else:
+            self.log('{} {} item {} linked to {}: {}'.format(
+                item.source, item.sub_source, item.sku, linn_item.sku,
+                linn_item.stock_id))
+            item.link(linn_item.stock_id)
+            self.linked_count += 1
