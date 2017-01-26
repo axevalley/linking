15b1ce693094213d1c34d627d67d5407a76d2d71
Luke Shiner <luke@lukeshiner.com>
2017-01-26 12:08:22 +0000

    V1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='linking',
-    version='1.0',
+    version='1.1',
     description='Manages Linnworks linking',
     author='Luke Shiner',
     install_requires=['requests'],
